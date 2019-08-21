5214a9cbc516074ead61ad1aa522dbd79ba49864
thiagodf <thiagodf@ciandt.com>
2019-08-21 10:23:34 -0300

    adiconando nome e idade

--- src/br/com/ciandt/thiagodf/operadores/Atribuicao.java
+++ src/br/com/ciandt/thiagodf/operadores/Atribuicao.java
@@ -4,6 +4,11 @@ public class Atribuicao {
 
 	public static void main(String[] args) {
 		
+		String nome = "João";
+		int idade = 26;
+		
+		// Print Nome:João, Idade:26 anos
+		System.out.println("Nome:" + nome + ", Idade:" + idade + " anos");
 	}
 
 }
