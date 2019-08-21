7ba15b9a72c4b2061644bb11f1fd11d350e0f93a
thiagodf <thiagodf@ciandt.com>
2019-08-21 13:37:52 -0300

    class igualdade

--- src/br/com/ciandt/thiagodf/operadores/Igualdade.java
+++ src/br/com/ciandt/thiagodf/operadores/Igualdade.java
@@ -1,9 +1,27 @@
 package br.com.ciandt.thiagodf.operadores;
 
+import java.util.Scanner;
+
 public class Igualdade {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite qual é o valor:");
+		double valor = sc.nextDouble();
+		
+		
+		if (valor == 10) {
+			// Se o valor digitado for igual a 10 ele imprime.
+			System.out.println("Acertou!!!");
+			
+		} else {
+			// Se o valor digitado for diferente de 10 ele imprime.
+			System.out.println("ERRROOOOUUU!!!");
+		}
 
 	}
 
