7a3432b9d137c3fb4ba302d0c818cef743396420
Jonatas WF <Jonataswf@msn.com>
2019-08-21 12:16:16 -0300

    commit das classes Igualdade, Logicos e Relacionais

--- src/br/com/ciandt/jwillian/operadores/Igualdade.java
+++ src/br/com/ciandt/jwillian/operadores/Igualdade.java
@@ -1,10 +1,22 @@
 package br.com.ciandt.jwillian.operadores;
 
+import java.util.Scanner;
+
 public class Igualdade {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite um numero: ");
+		double valor = sc.nextDouble();
+		
+		if (valor == 10) {
+			System.out.println("Acertou");
+		} else {
+			System.out.println("ERROOOOUUU");
+		}
+		sc.close();
 	}
 
 }
--- src/br/com/ciandt/jwillian/operadores/Logicos.java
+++ src/br/com/ciandt/jwillian/operadores/Logicos.java
@@ -1,10 +1,27 @@
 package br.com.ciandt.jwillian.operadores;
 
+import java.util.Scanner;
+
 public class Logicos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Entre com o primeiro número: ");
+		int primeiroNumero = sc.nextInt();
+		
+		System.out.println("Entre com o segundo número: ");
+		int segundoNumero = sc.nextInt();
+		
+		if (primeiroNumero > 100 && segundoNumero > 100) {
+			System.out.println("Os dois números são maiores que 100");
+		} else if (primeiroNumero > 100 || segundoNumero > 100){
+			System.out.println("Somente um número é maior que 100");
+		} else {
+			System.out.println("Os dois numeros são menores ou igual a 100");
+		}
+		sc.close();
 	}
 
 }
