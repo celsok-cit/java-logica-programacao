7cbdd58b691227d12310e9961d868095057e9f8e
thiagodf <thiagodf@ciandt.com>
2019-08-21 13:27:07 -0300

    class decremento

--- src/br/com/ciandt/thiagodf/operadores/Decremento.java
+++ src/br/com/ciandt/thiagodf/operadores/Decremento.java
@@ -1,9 +1,17 @@
 package br.com.ciandt.thiagodf.operadores;
 
+import java.util.Scanner;
+
 public class Decremento {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite o valor para decrementar no console:");
+		double valor = sc.nextDouble();
+		
+		System.out.println(--valor);
 	}
 
 }
