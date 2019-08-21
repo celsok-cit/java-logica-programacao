cf5cb0c7f81e95257ec23d211e75d9bf0d5f1fdc
thiagodf <thiagodf@ciandt.com>
2019-08-21 10:40:53 -0300

    calculo de metros por segundo

--- src/br/com/ciandt/thiagodf/operadores/Aritmetico.java
+++ src/br/com/ciandt/thiagodf/operadores/Aritmetico.java
@@ -4,6 +4,9 @@ public class Aritmetico {
 
 	public static void main(String[] args) {
 		
+		double segundos = 60*360;
+		double metros = 1000*457;
+		
+		System.out.println("João fez em "+ (metros/segundos) + " metros por segundo");
 	}
-
 }
