b0413eb9864942e786390ba82e75d8dfbd3fc1e9
Jonatas WF <Jonataswf@msn.com>
2019-08-22 14:30:53 -0300

    Reformulando o codigo do IfElse e Ternario

--- src/br/com/ciandt/jwillian/decisao/IfElse.java
+++ src/br/com/ciandt/jwillian/decisao/IfElse.java
@@ -1,5 +1,6 @@
 package br.com.ciandt.jwillian.decisao;
 
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class IfElse {
@@ -7,18 +8,19 @@ public class IfElse {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
+		int anoAtual = Calendar.getInstance().get(Calendar.YEAR);
 		
-		System.out.println("Digite o ano atual: ");
-		int anoAtual = sc.nextInt();
+		System.out.println("Ano Atual: " + anoAtual);
+	
 		
 		System.out.println("Digite o ano de nascimento: ");
 		int anoNascimento = sc.nextInt();
 		int idade = anoAtual - anoNascimento;
 		
 		if (idade >= 18) {
-			System.out.println("você tem " + idade + " anos, você é adulto!");
+			System.out.println("Você tem " + idade + " anos, você é adulto!");
 		} else {
-			System.out.println("você tem " + idade + " anos, você é menor de idade!");
+			System.out.println("Você tem " + idade + " anos, você é menor de idade!");
 		}		
 		sc.close();
 	}
--- src/br/com/ciandt/jwillian/decisao/Ternario.java
+++ src/br/com/ciandt/jwillian/decisao/Ternario.java
@@ -1,17 +1,18 @@
 package br.com.ciandt.jwillian.decisao;
 
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class Ternario {
 
 	public static void main(String[] args) {
 		
-Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
+		int anoAtual = Calendar.getInstance().get(Calendar.YEAR);
 		
-		System.out.println("Digite o ano atual: ");
-		int anoAtual = sc.nextInt();
+		System.out.println("Ano Atual: " + anoAtual);
 		
-		System.out.println("Digite o ano de nascimento: ");
+		System.out.println("Digite o ano de nacimento: ");
 		int anoNascimento = sc.nextInt();
 		int idade = anoAtual - anoNascimento;
 		
