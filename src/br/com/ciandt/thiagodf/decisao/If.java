95beaa30cb3bf3440b7a0003abf0bba409108cd9
thiagodf <thiagodf@ciandt.com>
2019-08-25 14:25:17 -0300

    class if

--- src/br/com/ciandt/thiagodf/decisao/If.java
+++ src/br/com/ciandt/thiagodf/decisao/If.java
@@ -1,10 +1,24 @@
 package br.com.ciandt.thiagodf.decisao;
 
+import java.util.Scanner;
+
 public class If {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite o valor:");
+		int valor = sc.nextInt();
+		
+		if (valor > 25) {
+			
+			System.out.println("Desconto");
+			
+		}
+		System.out.println("Obrigado pela compra");
 	}
 
 }
