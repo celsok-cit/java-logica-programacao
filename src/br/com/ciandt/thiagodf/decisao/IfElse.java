08e9fa8c974f38337c896bfb14a2cbdcc5efd226
thiagodf <thiagodf@ciandt.com>
2019-08-25 14:56:45 -0300

    class ifElse

--- src/br/com/ciandt/thiagodf/decisao/IfElse.java
+++ src/br/com/ciandt/thiagodf/decisao/IfElse.java
@@ -1,8 +1,28 @@
 package br.com.ciandt.thiagodf.decisao;
 
+import java.util.Calendar;
+import java.util.Scanner;
+
 public class IfElse {
 	
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite os 4 digitos do seu ano de nascimento:");
+		int anoNascimento = sc.nextInt();
+		int anoAtual = Calendar.getInstance().get(Calendar.YEAR);
+		int resultado = anoAtual - anoNascimento;
+		
+		if (resultado >= 18) {
+			
+			System.out.println("Adulto!.");
+			
+		} else {
+			
+			System.out.println("Menor de idade!.");
+			
+		}
 	}
 }
