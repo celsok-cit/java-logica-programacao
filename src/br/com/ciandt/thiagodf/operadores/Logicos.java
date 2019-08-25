861301d8bc473eeb82865c065655b79a49b87e83
thiagodf <thiagodf@ciandt.com>
2019-08-25 14:13:32 -0300

    class logicos

--- src/br/com/ciandt/thiagodf/operadores/Logicos.java
+++ src/br/com/ciandt/thiagodf/operadores/Logicos.java
@@ -1,9 +1,33 @@
 package br.com.ciandt.thiagodf.operadores;
 
+import java.util.Scanner;
+
 public class Logicos {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Digite o primeiro número:");
+		int primeiroNumero = sc.nextInt();
+
+		System.out.println("Digite o segundo número:");
+		int segundoNumero = sc.nextInt();
+
+		if (primeiroNumero > 100 && segundoNumero > 100) {
+
+			System.out.println("Os dois número são maiores que 100");
+
+		} else {
+			
+			if (primeiroNumero > 100 || segundoNumero > 100) {
+
+				System.out.println("Somente um número é maior que 100");
+			}
+			
+		}
 
 	}
 
--- src/br/com/ciandt/thiagodf/operadores/Relacionais.java
+++ src/br/com/ciandt/thiagodf/operadores/Relacionais.java
@@ -1,10 +1,39 @@
 package br.com.ciandt.thiagodf.operadores;
 
+import java.util.Scanner;
+
 public class Relacionais {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int notaUm = 0;
+		int notaDois = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		if (notaUm == 0 && notaDois == 0) {
+			
+			System.out.println("Digite sua primeira nota:");
+			notaUm = sc.nextInt();
+			
+			System.out.println("Digite sua segunda nota:");
+			notaDois = sc.nextInt();
+			
+			int media = (notaUm + notaDois) / 2;
+			
+			
+			if (media >= 7) {
+				
+				System.out.println("Média: " + media + " Aprovado!!!");
+				
+			} 
+			
+			if (media < 4) {
+				
+				System.out.println("Média: " + media + " Reprovado!!!");
+				
+			}
+		} 
 	}
-
 }
